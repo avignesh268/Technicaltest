b8f28c8d779085563e01319c3c3d4a12e4e18b80
Ashwin Saravanan <31976338+avignesh268@users.noreply.github.com>
2019-10-25 07:50:37 +1100

    Question 1 Updated.
    This is the correct code for the Part 1

--- jokes.py
+++ jokes.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 import requests
 import json
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 
 @app.route('/getJokes')
 def homepage():
-  r = requests.get(
-      'http://api.icndb.com/jokes/random/')
-  return render_template('jokes.html', movies=json.loads(r.text)['jokes'])
+    my_list =[]
+    for x in range(10):
+        r = requests.get('http://api.icndb.com/jokes/random/')
+        my_list.append(json.loads(r.text)['value'])
+
+    return render_template('the.html', my_list1=my_list)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(host='0.0.0.0', debug=True)
